13c5f8323daa66a1315ca80b4cf3e24a0eaf409f
leonard <40259905+leonard246@users.noreply.github.com>
2019-11-25 14:14:47 +1100

    scraper function added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
+var cheerio = require('cheerio'); 
+
+function scrape(uri) {
+  var options = {
+    uri: 'https://www.blackwolf.com.au/products/categories/clearance/',
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+  
+  rp(options)
+  .then(function ($) {
+    console.log($('.title-prd').text())
+    console.log($('.bc-product__original-price').text())
+    console.log($('.bc-product__price').text())    
+  })
+  .catch(function (err) {
+    console.log(`Failed to crawl: ${err}`)
+  });
+}
+
+scrape();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Outdoor Clearance Gear Finder' });
+  res.render('index', { title: 'Clearance Outdoor Gear' });
 });
 
 module.exports = router;
