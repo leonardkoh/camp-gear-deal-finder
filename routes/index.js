3d0e9ee200c2bcf7be2c9c4f40a89faf85865dce
leonard <40259905+leonard246@users.noreply.github.com>
2019-11-25 13:58:49 +1100

    naming changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Camping  ' });
+  res.render('index', { title: 'Outdoor Clearance Gear Finder' });
 });
 
 module.exports = router;
--- urlScrapeList.js
+++ urlScrapeList.js
@@ -1,4 +1,4 @@
 const urlList = [ "https://www.blackwolf.com.au/products/categories/clearance/",
 "https://www.blackwolf.com.au/products/categories/clearance/?fwp_paged=2",
-
+"https://decathlon.com.au/collections/crazy-bargains"
 ];
