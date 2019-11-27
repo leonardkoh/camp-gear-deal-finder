168965326471c1147b21c74e24491a38b05f3736
leonard <40259905+leonard246@users.noreply.github.com>
2019-11-27 13:13:58 +1100

    function added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var rp = require('request-promise');
 var cheerio = require('cheerio'); 
 
-function scrape(uri) {
+let blackWolfProducts = [];
+
+function scrapeBlackwolf() {
   var options = {
     uri: 'https://www.blackwolf.com.au/products/categories/clearance/',
     transform: function (body) {
@@ -13,16 +15,32 @@ function scrape(uri) {
   
   rp(options)
   .then(function ($) {
-    console.log($('.title-prd').text())
-    console.log($('.bc-product__original-price').text())
-    console.log($('.bc-product__price').text())    
+    let titles = $('.title-prd').text().split('\t').filter(e => { return e.match(/\S/); });
+    let originalPrices = $('.bc-product__original-price').text().split(/[$]/); 
+      originalPrices.shift();
+    let salePrices = $('.bc-product__price').text().split('\t').filter(e => { return e.match(/\S/); });
+
+    for(let i=0; i<title.length; i++) {
+      let product = {
+        title: titles[i],
+        origPrice: originalPrices[i],
+        salePrice: salePrices[i],
+      };
+      blackWolfProducts.push(product);  
+    }
+
+    // console.log(titles);
+    // console.log(originalPrices);
+    // console.log(salePrices);
+
+    console.log(blackWolfProducts)
   })
   .catch(function (err) {
-    console.log(`Failed to crawl: ${err}`)
+    console.log(`${err}`)
   });
 }
 
-scrape();
+scrapeBlackwolf();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
