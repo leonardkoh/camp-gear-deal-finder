858c9937b2e1787474da24f5633e0d7510d171a7
leonard <40259905+leonard246@users.noreply.github.com>
2019-11-29 22:27:44 +1100

    blackwolf complete + style changes

--- routes/index.js
+++ routes/index.js
@@ -15,19 +15,26 @@ function scrapeBlackwolf() {
   
   rp(options)
   .then(function ($) {
+
     let titles = $('.title-prd').text().split('\t').filter(e => { return e.match(/\S/); });
     let originalPrices = $('.bc-product__original-price').text().split(/[$]/); 
       originalPrices.shift();
     let salePrices = $('.bc-product__price').text().split('\t').filter(e => { return e.match(/\S/); });
+    let prodImages = [];
+    let prodURLs = [];
 
+    $('.bc-product-card__featured-image').each((i,e)=>{
+        prodImages.push($(e).find('img').attr('src'));
+        prodURLs.push($(e).find('a').attr('href'));
+    })
     for(let i=0; i<titles.length; i++) {
       bwp.push({ title: titles[i],
         origPrice: originalPrices[i],
-        salePrice: salePrices[i]
+        salePrice: salePrices[i],
+        prodImage: prodImages[i],
+        prodURL: prodURLs[i]
       })
     }
-
-    console.log(bwp)
   })
   .catch(function (err) {
     console.log(`${err}`)
